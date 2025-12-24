3612a8852bec65e38a82f8e4d65083c2dbada79f
Arvind Singh <arvind42005@gmail.com>
2025-12-25 00:45:26 +0530

    initialize the project

---
+++ portfolio/app/layout.js
@@ -0,0 +1,17 @@
+// app/layout.js
+
+export const metadata = {
+    title: "My Portfolio",
+    description: "Personal portfolio website",
+  };
+  
+  export default function RootLayout({ children }) {
+    return (
+      <html lang="en">
+        <body>
+          {children}
+        </body>
+      </html>
+    );
+  }
+  
