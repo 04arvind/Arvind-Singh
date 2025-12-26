9eebf0e89ba239a56823b4c8e618e316777c6183
Arvind Singh <arvind42005@gmail.com>
2025-12-26 21:17:29 +0530

    add files

---
+++ portfolio/app/blogs/page.js
@@ -0,0 +1,10 @@
+import Common from "@/components/common";
+import BlogSection from "@/components/blogSection";
+export default async function Blogs() {
+  return (
+    <main className="flex items-center justify-between w-full flex-col p-8 min-h-screen">
+      <Common />
+      <BlogSection/>
+    </main>
+  );
+}
--- portfolio/app/page.js
+++ portfolio/app/page.js
@@ -1,5 +1,11 @@
+import Common from "@/components/common";
+import AboutSection from "@/components/aboutSection";
+
 export default function Home() {
   return (
-    <div>hello</div>
+    <main className="flex items-center justify-between w-full flex-col p-8 min-h-screen">
+      <Common />
+      <AboutSection />
+    </main>
   );
 }
